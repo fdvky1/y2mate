67cbc7daba374165f0a87fe21ea200b7c162eb4c
Fdvky1 <fdvkey@gmail.com>
2023-06-30 09:07:30 +0700

    First Commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module y2mate
+
+go 1.20
+
+require (
+	github.com/EDDYCJY/fake-useragent v0.2.0
+	github.com/go-resty/resty/v2 v2.7.0
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	golang.org/x/net v0.7.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	browser "github.com/EDDYCJY/fake-useragent"
+	"github.com/go-resty/resty/v2"
+)
+
+
+type Y2MateRes struct{
+	Status    string `json:"status"`
+	Mess      string `json:"mess"`
+	Page      string `json:"page"`
+	Vid       string `json:"vid"`
+	Extractor string `json:"extractor"`
+	Title     string `json:"title"`
+	T         int    `json:"t"`
+	A         string `json:"a"`
+	Links     struct{
+		Mp4 map[string]struct{
+			Size  string `json:"size"`
+			F     string `json:"f"`
+			Q     string `json:"q"`
+			QText string `json:"q_text"`
+			K     string `json:"k"`
+		}
+		Mp3 map[string]struct{
+			Size  string `json:"size"`
+			F     string `json:"f"`
+			Q     string `json:"q"`
+			QText string `json:"q_text"`
+			K     string `json:"k"`
+		}
+	} `json:"links"`
+}
+
+type Y2MateCVRes struct {
+	Status   string `json:"status"`
+	Mess     string `json:"mess"`
+	CStatus  string `json:"c_status"`
+	Vid      string `json:"vid"`
+	Title    string `json:"title"`
+	Ftype    string `json:"ftype"`
+	Fquality string `json:"fquality"`
+	Dlink    string `json:"dlink"`
+}
+
+func Get(url string) (result *Y2MateRes, err error){
+	client := resty.New()
+	res, err := client.R().
+				SetFormData(map[string]string{
+					"k_query": url,
+					"k_page": "home",
+					"hl": "en",
+					"q_auto": "0",
+				}).
+				SetHeader("Origin", "https://www.y2mate.com").
+				SetHeader("Referer", "https://www.y2mate.com").
+				SetHeader("USer-Agent", browser.MacOSX()).
+				Post("https://www.y2mate.com/mates/analyzeV2/ajax")
+	if err != nil {
+		return nil, err
+	}
+	defer res.RawBody().Close()
+
+	var R Y2MateRes
+
+	err = json.Unmarshal(res.Body(), &R)
+	
+	if err != nil {
+		return nil, err
+	}
+	
+	return &R, nil
+}
+
+func GetDownloadUrl(vid string, key string) (result *Y2MateCVRes, err error){
+	client := resty.New()
+	res, err := client.R().
+				SetFormData(map[string]string{
+					"vid": vid,
+					"k": key,
+				}).
+				SetHeader("Origin", "https://www.y2mate.com").
+				SetHeader("Referer", "https://www.y2mate.com/youtube/"+vid).
+				SetHeader("USer-Agent", browser.MacOSX()).
+				Post("https://www.y2mate.com/mates/convertV2/index")
+	if err != nil {
+		return nil, err
+	}
+	defer res.RawBody().Close()
+
+	var R Y2MateCVRes
+
+	err = json.Unmarshal(res.Body(), &R)
+	if err != nil {
+		return nil, err
+	}
+
+	return &R, nil
+}
+
+func main(){
+	result, err := Get("https://youtu.be/ioW9iGDpQyw")
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, v := range result.Links.Mp3 {
+		url, err := GetDownloadUrl(result.Vid, v.K)
+		if err != nil {
+			fmt.Println(err)
+		}	
+		fmt.Println(url.Dlink)
+	}
+}
+
